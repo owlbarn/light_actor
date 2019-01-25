174574d5e4df3e984279b955dbf3154faa2160b7
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-01-25 12:02:39 +0000

    test_0: try to get IP of uuid via DNS if any
    This is a preparation for the following docker'ization, where each
    service runs on a different node(different IP address) via docker.
    
    This tries to get an IP address of UUID via DNS if it's available,
    which is the case of docker(-compose) DNS. It falls down to the
    original "localhost"(127.0.0.1) if DNS doesn't find an IP of UUID as
    it used to be.
    
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- test/actor_param_test_0.ml
+++ test/actor_param_test_0.ml
@@ -60,13 +60,23 @@ include Actor_param_types.Make(Impl)
 module M = Actor_param.Make (Actor_net_zmq) (Actor_sys_unix) (Impl)
 
 
+let ip_of_uuid id =
+  try
+    (Unix.gethostbyname id).h_addr_list.(0)
+    |> Unix.string_of_inet_addr
+  with Not_found -> "127.0.0.1"
+
 let main args =
   Actor_log.(set_level DEBUG);
   Random.self_init ();
 
   (* define server uuid and address *)
   let server_uuid = "server" in
-  let server_addr = "tcp://127.0.0.1:5555" in
+  let server_port =
+    try Unix.getenv "SERVER_PORT"
+    with Not_found -> "5555" in
+  let server_addr = "tcp://" ^ (ip_of_uuid  server_uuid) ^
+                    ":" ^ server_port in
 
   (* define my own uuid and address *)
   let my_uuid = args.(1) in
@@ -74,8 +84,11 @@ let main args =
     if my_uuid = server_uuid then
       server_addr
     else
-      let port = string_of_int (6000 + Random.int 1000) in
-      "tcp://127.0.0.1:" ^ port
+      let port = try Unix.getenv "PORT"
+        with Not_found ->
+          string_of_int (6000 + Random.int 1000)
+      in
+      "tcp://" ^ (ip_of_uuid my_uuid) ^ ":" ^ port
   in
 
   (* define the participants *)
