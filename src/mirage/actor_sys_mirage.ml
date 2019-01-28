270bf9c74495fae4f40c15558435837e3ddac44f
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-01-28 08:12:16 +0000

    mirage: sleep: no dependency on unix
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- src/mirage/actor_sys_mirage.ml
+++ src/mirage/actor_sys_mirage.ml
@@ -3,4 +3,5 @@
  * Copyright (c) 2016-2019 Liang Wang <liang.wang@cl.cam.ac.uk>
  *)
 
-let sleep = Lwt_unix.sleep
+let sleep delay =
+  Mirage_OS.OS.Time.sleep_ns (Duration.of_sec (int_of_float delay))
